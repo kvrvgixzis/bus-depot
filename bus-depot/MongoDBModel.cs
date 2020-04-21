ef4756abe506d7bf233ac191b0815c396fb951a2
Nikita parfenov <n1koss.boy@gmail.com>
2020-04-21 17:18:10 +0500

    First commit

---
+++ bus-depot/MongoDBModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace bus_depot
+{
+    public partial class Driver
+    {
+        /*
+         * id,
+         * ФИО,
+         * класс,
+         * стаж,
+         * оклад (вычисляемое),
+         * автобус (id),
+         * маршрут (id),
+         * график (список дней недели).
+         */
+        [BsonId]
+        public ObjectId Id { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Name { get; set; }
+
+        public string Patronymic { get; set; }
+
+        public int Grade { get; set; }
+
+        public int Experience { get; set; }
+
+        public float Salary { get; set; }
+
+        public ObjectId BusId { get; set; }
+
+        public ObjectId RouteId { get; set; }
+
+        public List<string> Schedule { get; set; }
+
+        public Driver()
+        {
+            Id = ObjectId.GenerateNewId();
+            Schedule = new List<string>();
+        }
+    }
+
+    
+}
