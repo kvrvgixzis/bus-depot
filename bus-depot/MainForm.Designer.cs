f39e3ffb3ad606159162b4fd325d65b1de1befbb
Nikita parfenov <n1koss.boy@gmail.com>
2020-04-23 21:54:16 +0500

    adding show table, adding show buses, routes and drivers methods

--- bus-depot/MainForm.Designer.cs
+++ bus-depot/MainForm.Designer.cs
@@ -28,20 +28,159 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.leftControlPanel = new System.Windows.Forms.Panel();
+            this.Table = new System.Windows.Forms.DataGridView();
+            this.ShowBusesBtn = new System.Windows.Forms.Button();
+            this.ShowDriversBtn = new System.Windows.Forms.Button();
+            this.ShowRoutesBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.AddNewElementBtn = new System.Windows.Forms.Button();
+            this.deleteSelectedBtn = new System.Windows.Forms.Button();
+            this.leftControlPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).BeginInit();
             this.SuspendLayout();
+            // 
+            // leftControlPanel
+            // 
+            this.leftControlPanel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.leftControlPanel.Controls.Add(this.deleteSelectedBtn);
+            this.leftControlPanel.Controls.Add(this.ShowRoutesBtn);
+            this.leftControlPanel.Controls.Add(this.AddNewElementBtn);
+            this.leftControlPanel.Controls.Add(this.ShowDriversBtn);
+            this.leftControlPanel.Controls.Add(this.ExitBtn);
+            this.leftControlPanel.Controls.Add(this.ShowBusesBtn);
+            this.leftControlPanel.Location = new System.Drawing.Point(2, 2);
+            this.leftControlPanel.Name = "leftControlPanel";
+            this.leftControlPanel.Size = new System.Drawing.Size(209, 447);
+            this.leftControlPanel.TabIndex = 0;
+            // 
+            // Table
+            // 
+            this.Table.AllowUserToAddRows = false;
+            this.Table.AllowUserToDeleteRows = false;
+            this.Table.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Table.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Table.BackgroundColor = System.Drawing.Color.White;
+            this.Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Table.Location = new System.Drawing.Point(217, 127);
+            this.Table.MultiSelect = false;
+            this.Table.Name = "Table";
+            this.Table.ReadOnly = true;
+            this.Table.RowHeadersVisible = false;
+            this.Table.RowTemplate.Height = 24;
+            this.Table.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Table.ShowEditingIcon = false;
+            this.Table.Size = new System.Drawing.Size(571, 311);
+            this.Table.TabIndex = 1;
+            // 
+            // ShowBusesBtn
+            // 
+            this.ShowBusesBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowBusesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowBusesBtn.Location = new System.Drawing.Point(3, 125);
+            this.ShowBusesBtn.Name = "ShowBusesBtn";
+            this.ShowBusesBtn.Size = new System.Drawing.Size(203, 32);
+            this.ShowBusesBtn.TabIndex = 0;
+            this.ShowBusesBtn.Text = "Buses";
+            this.ShowBusesBtn.UseVisualStyleBackColor = true;
+            this.ShowBusesBtn.Click += new System.EventHandler(this.ShowBusesBtn_Click);
+            // 
+            // ShowDriversBtn
+            // 
+            this.ShowDriversBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowDriversBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowDriversBtn.Location = new System.Drawing.Point(3, 163);
+            this.ShowDriversBtn.Name = "ShowDriversBtn";
+            this.ShowDriversBtn.Size = new System.Drawing.Size(203, 32);
+            this.ShowDriversBtn.TabIndex = 1;
+            this.ShowDriversBtn.Text = "Drivers";
+            this.ShowDriversBtn.UseVisualStyleBackColor = true;
+            this.ShowDriversBtn.Click += new System.EventHandler(this.ShowDriversBtn_Click);
+            // 
+            // ShowRoutesBtn
+            // 
+            this.ShowRoutesBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ShowRoutesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ShowRoutesBtn.Location = new System.Drawing.Point(3, 201);
+            this.ShowRoutesBtn.Name = "ShowRoutesBtn";
+            this.ShowRoutesBtn.Size = new System.Drawing.Size(203, 32);
+            this.ShowRoutesBtn.TabIndex = 2;
+            this.ShowRoutesBtn.Text = "Routes";
+            this.ShowRoutesBtn.UseVisualStyleBackColor = true;
+            this.ShowRoutesBtn.Click += new System.EventHandler(this.ShowRoutesBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ExitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ExitBtn.Location = new System.Drawing.Point(3, 404);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(203, 32);
+            this.ExitBtn.TabIndex = 3;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // AddNewElementBtn
+            // 
+            this.AddNewElementBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddNewElementBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddNewElementBtn.Location = new System.Drawing.Point(3, 255);
+            this.AddNewElementBtn.Name = "AddNewElementBtn";
+            this.AddNewElementBtn.Size = new System.Drawing.Size(203, 32);
+            this.AddNewElementBtn.TabIndex = 4;
+            this.AddNewElementBtn.Text = "Add new bus";
+            this.AddNewElementBtn.UseVisualStyleBackColor = true;
+            // 
+            // deleteSelectedBtn
+            // 
+            this.deleteSelectedBtn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteSelectedBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.deleteSelectedBtn.Location = new System.Drawing.Point(3, 293);
+            this.deleteSelectedBtn.Name = "deleteSelectedBtn";
+            this.deleteSelectedBtn.Size = new System.Drawing.Size(203, 32);
+            this.deleteSelectedBtn.TabIndex = 5;
+            this.deleteSelectedBtn.Text = "Delete selected bus";
+            this.deleteSelectedBtn.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Table);
+            this.Controls.Add(this.leftControlPanel);
+            this.MinimumSize = new System.Drawing.Size(816, 485);
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "BUS DEPOT TERMIANL";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.leftControlPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Table)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel leftControlPanel;
+        private System.Windows.Forms.Button AddNewElementBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.Button ShowRoutesBtn;
+        private System.Windows.Forms.Button ShowDriversBtn;
+        private System.Windows.Forms.Button ShowBusesBtn;
+        private System.Windows.Forms.DataGridView Table;
+        private System.Windows.Forms.Button deleteSelectedBtn;
     }
 }
--- bus-depot/MainForm.cs
+++ bus-depot/MainForm.cs
@@ -13,6 +13,106 @@ namespace bus_depot
     public partial class MainForm : Form
     {
         MongoTools database;
+        private void ShowRoutes()
+        {
+            Table.Rows.Clear();
+            Table.Columns.Clear();
+            Table.Refresh();
+
+            var documents = database.LoadAllDocuments<Route>("routes");
+
+            Table.ColumnCount = 7;
+
+            Table.Columns[0].Name = "Number";
+            Table.Columns[1].Name = "Start Point";
+            Table.Columns[2].Name = "End Point";
+            Table.Columns[3].Name = "Start Time";
+            Table.Columns[4].Name = "End Time";
+            Table.Columns[5].Name = "Interval";
+            Table.Columns[6].Name = "Length";
+
+            foreach (var doc in documents)
+            {
+                string number = Convert.ToString(doc.Number);
+                string stPoint = Convert.ToString(doc.StPoint);
+                string endPoint = Convert.ToString(doc.EndPoint);
+                string stTime = Convert.ToString(doc.StTime);
+                string endTime = Convert.ToString(doc.EndTime);
+                string interval = Convert.ToString(doc.Interval);
+                string lenght = Convert.ToString(doc.Length);
+
+                string[] row = { number, stPoint, endPoint, stTime, endTime, interval, lenght };
+                
+                Table.Rows.Add(row);
+            }
+        }
+        private void ShowBuses()
+        {
+            Table.Rows.Clear();
+            Table.Columns.Clear();
+            Table.Refresh();
+
+            var documents = database.LoadAllDocuments<Bus>("buses");
+
+            Table.ColumnCount = 5;
+
+            Table.Columns[0].Name = "Number";
+            Table.Columns[1].Name = "Type";
+            Table.Columns[2].Name = "Capacity";
+            Table.Columns[3].Name = "Driver";
+            Table.Columns[4].Name = "Is Working";
+
+            foreach (var doc in documents)
+            {
+                string number = doc.Number;
+                string type = doc.Type;
+                string capacity = Convert.ToString(doc.Сapacity);
+                string driver = (Convert.ToString(doc.DriverId) == "000000000000000000000000") ? "-" : Convert.ToString(doc.DriverId);
+                string isWorking = Convert.ToString(doc.IsWorking);
+
+                string[] row = { number, type, capacity, driver, isWorking };
+
+                Table.Rows.Add(row);
+            }
+        }
+
+        private void ShowDrivers()
+        {
+            Table.Rows.Clear();
+            Table.Columns.Clear();
+            Table.Refresh();
+
+            var documents = database.LoadAllDocuments<Driver>("drivers");
+
+            Table.ColumnCount = 7;
+
+            Table.Columns[0].Name = "Full Name";
+            Table.Columns[1].Name = "Grade";
+            Table.Columns[2].Name = "Experience";
+            Table.Columns[3].Name = "Salary";
+            Table.Columns[4].Name = "Bus";
+            Table.Columns[5].Name = "Route";
+            Table.Columns[6].Name = "Schedule";
+
+            foreach (var doc in documents)
+            {
+                string fullName = $"{doc.LastName} {doc.Name} {doc.Patronymic}";
+                string grade = Convert.ToString(doc.Grade);
+                string exp = Convert.ToString(doc.Experience);
+                string sal = Convert.ToString(doc.Salary);
+                string bus = (Convert.ToString(doc.BusId) == "000000000000000000000000") ? "-" : Convert.ToString(doc.BusId);
+                string route = (Convert.ToString(doc.RouteId) == "000000000000000000000000") ? "-" : Convert.ToString(doc.RouteId);
+                string schedule = "";
+                foreach (var day in doc.Schedule)
+                {
+                    schedule += $" {day}";
+                }
+
+                string[] row = { fullName, grade, exp, sal, bus, route, schedule };
+
+                Table.Rows.Add(row);
+            }
+        }
         public MainForm(MongoTools database)
         {
             this.database = database;
@@ -22,6 +122,34 @@ namespace bus_depot
         private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ShowBuses();
+        }
+
+        private void ShowBusesBtn_Click(object sender, EventArgs e)
+        {
+            ShowBuses();
+            AddNewElementBtn.Text = "Add new bus";
+        }
+
+        private void ShowRoutesBtn_Click(object sender, EventArgs e)
+        {
+            ShowRoutes();
+            AddNewElementBtn.Text = "Add new route";
+        }
+
+        private void ShowDriversBtn_Click(object sender, EventArgs e)
+        {
+            ShowDrivers();
+            AddNewElementBtn.Text = "Add new driver";
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
         }
     }
 }
